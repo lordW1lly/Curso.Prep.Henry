6410a75a7b4bb70f9763f7653738da1040699c9c
Aleengelstajn <sebastianengelstajn@gmail.com>
2021-10-12 12:15:32 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -165,7 +165,7 @@ return arrayTabladelSeis
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero.length===3) {return true}; return false
 }
 
 function doWhile(numero) {
