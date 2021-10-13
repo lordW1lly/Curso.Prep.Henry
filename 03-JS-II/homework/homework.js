c55e7e2b23ad2bbfab96e0892fc34418df84ffcd
[lordW1lly] <sebastianengelstajn@gmail.com>
2021-10-13 00:48:49 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -114,22 +114,21 @@ function operadoresLogicos(num1, num2, num3) {
   
   if(num1>num2 && num1>num3 && num1>0) { 
     return "Número 1 es mayor y positivo";
-  };
+  }
 
-  if(num1 < 0 || num2 < 0 || num3 < 0) {
+  if(num1<0 || num2<0 || num3<0) {
     return "Hay negativos";
-  };
+  }
 
  if(num3>num1 && num3>num2) {
     return num3+1;  
-  };
+  }
  if(num1==0 || num2==0 || num3 ==0) {
     return "Error";
-  };
-
+  }
   return false
-} 
-  
+}
+
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
@@ -138,10 +137,12 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
 if (numero<2) {return false};
 if (numero===2) {return true};
-for (var i=2; i<numero; i++) {
-  if(numero%i===0) {return false};
-  return true
+for (var i=2; i<numero; i++) 
+{
+if(numero%i===0) {return false};
+  
 }
+return true
 }
 
 function esVerdadero(valor){
@@ -156,8 +157,8 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   let arrayTabladelSeis = []
-  for(let i = 0; i < 11; i++); {
-    arrayTabladelSeis.push(6*1)
+  for(let i = 0; i < 11; i++) {
+    arrayTabladelSeis.push(6*i)
   }
 return arrayTabladelSeis
 }
@@ -165,7 +166,7 @@ return arrayTabladelSeis
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero.length===3) {return true}; return false
+  if (numero > 99 && numero<1000) {return true}; return false
 }
 
 function doWhile(numero) {
