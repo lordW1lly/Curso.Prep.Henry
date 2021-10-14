94d3e62249f8282890c5e07079855091ce10c4dd
[lordW1lly] <sebastianengelstajn@gmail.com>
2021-10-14 11:30:27 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -115,15 +115,15 @@ function operadoresLogicos(num1, num2, num3) {
   if(num1>num2 && num1>num3 && num1>0) { 
     return "Número 1 es mayor y positivo";
   }
-  if(num1 < 0 || num2 < 0 || num3 < 0) {
-    return "Hay negativos";
+  else if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return " Hay negativos ";
   }
-
- if(num3>num1 && num3>num2) {
-    return num3+1;  
+  else if(num3>num1 && num3>num2) {
+    return num3 + 1;  
   }
- if ( num1 === 0 || num2 === 0 || num3 === 0) {
-  return "Error";
+  else if ( num1 === 0 || num2 === 0 || num3 === 0) 
+ {
+  return " Error ";
   }
   return false;
 }
