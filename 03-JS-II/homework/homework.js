ecd2aa3aefc1705be9edf7450e59f5e795f0dce1
[lordW1lly] <sebastianengelstajn@gmail.com>
2021-10-17 01:57:35 -0300

    

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -115,8 +115,9 @@ function operadoresLogicos(num1, num2, num3) {
   if (num1 < 0 || num2 < 0 || num3 < 0) { return "Hay negativos";};
    if (num1 > num2 && num1 > num3 && num1 > 0) {return "Número 1 es mayor y positivo";};
    if (num3 > num1 && num3 > num2) {return num3 + 1};
-   if (num1 === 0  && num2 === 0 && num3 ===0) { return "Error"};
-  return false
+   if (num1 === 0 || num2 === 0 || num3 === 0) { return "Error"};
+  return false;
+
 }
 
 function esPrimo(numero) {
