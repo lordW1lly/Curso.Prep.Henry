9878f867de7ca7d0ab7305d365a05db550145777
[lordW1lly] <sebastianengelstajn@gmail.com>
2021-10-21 12:42:01 -0300

    05-JS-IV/homework/homework.js

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -1,11 +1,19 @@
 // No cambies los nombres de las funciones.
 
 function crearGato(nombre, edad) {
-  // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
-  // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
-  // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
-  // Devuelve el objeto
-  // Tu código:
+    // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
+    // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
+    // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
+    // Devuelve el objeto
+    // Tu código:
+    var gatito = {
+        nombre: nombre,
+        edad: edad,
+        meow: function() {
+            return "Meow!";
+        }
+    };
+    return gatito
 }
 
 
@@ -14,6 +22,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+    objeto[property] = null;
+    return objeto
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,13 +31,18 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+    objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
-  // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
-  // Multiplica el numeroMisterioso por 5 y devuelve el producto
-  // Tu código:
-
+    // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
+    // Multiplica el numeroMisterioso por 5 y devuelve el producto
+    // Tu código:
+    const objeto = {
+        numeroMisterioso: 2,
+    };
+    var producto = 5;
+    return producto * objetoMisterioso.numeroMisterioso
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
